0bc7b39be6d85ca44c92120716bcb0118079acc4
huntermb <bashawhm@clarkson.edu>
2017-11-05 17:45:57 -0500

    The next Update.
    Added diamonds, and smug Melania Trump

--- 7HRL.cpp
+++ 7HRL.cpp
@@ -10,6 +10,8 @@ int main(){
     Dungeon d;
     system("clear");
 
+    cout << "You must have a toilet plated with 7 gold layers to beat Melania Trump\nBuy more monsters to earn money faster!\n";
+
     while(d.toiletLevel() < 7){
         string input;
         d.printScreen();
@@ -17,11 +19,26 @@ int main(){
         if (!d.parse(input)){
             system("clear");
         } 
-        
+
         d.turn();
         
     }
-    cout<<"Congrats!!!! You Did It!!! You have an even golder toilet than Melania Trump!\n";
+    d.act1();
+    cout << "Gold toilets are going out of fashion.\nMelania moves on to diamond encrusted toilets.\n\n\n\n";
+
+    while (!d.isDone()){
+        string input;
+        d.printScreen();
+        getline(cin, input);
+        if (!d.parse(input)){
+            system("clear");
+        } 
+
+        d.turn();
+
+
+    }
+    cout<<"Congrats!!!! You Did It!!! You have an even more fashionable toilet than Melania Trump!\n";
 
     return 0;
 }
--- 7HRL.h
+++ 7HRL.h
@@ -11,18 +11,32 @@ class Dungeon{
 public:
     Dungeon(){
         srand(time(NULL));
+        toiletPrice = 40;
+        monsterPrice = 40;
+        diamondPrice = 80;
         money = 50;
         humansKilled = 0;
         toiletGold = 0;
         monsters = 4;
+        diamonds = 0;
+        act = 0;
     }
     void printScreen(){
-        cout << "Money $ " << money << "    Puny Humans Killed " << humansKilled << "    Toilet Gold Level " << toiletGold << "    Monsters " << monsters << "\n";
-        cout << "----------------------------------------------------------------------------\n";
-        cout << "Take Turn[t]  |  Buy Even Golder Toilet ($40) [g]  |  Buy more monsters ($40) [m]  |  Quit[q]\n";
-
+        if (act == 0){
+            cout << "Money $ " << money << "    Puny Humans Killed " << humansKilled << "    Toilet Gold Level " << toiletGold << "    Monsters " << monsters << "\n";
+            cout << "----------------------------------------------------------------------------\n";
+            cout << "Take Turn[t]  |  Buy Even Golder Toilet ($" << monsterPrice << ") [g]  |  Buy more monsters ($" << monsterPrice <<") [m]  |  Quit[q]\n";
+        } else {
+            cout << "Money $ " << money << "    Puny Humans Killed " << humansKilled << "    Toilet Gold Level " << toiletGold << "    Monsters " << monsters << "    Diamonds " << diamonds << "\n";
+            cout << "----------------------------------------------------------------------------\n";
+            cout << "Take Turn[t]  |   Buy diamonds ($" << diamondPrice << ")[d]  |  Buy more monsters ($" << monsterPrice <<") [m]  |  Quit[q]\n";
+       
+        }
     }
 
+    void act1(){
+        act = 1;
+    }
 
     //false if failed, true if success
     bool withdraw(int price){
@@ -35,27 +49,70 @@ public:
     }
 
     bool parse(string input){
-        if (input == "t"){
-            return false;
-        } else if (input == "g"){
-            if (!withdraw(40)){
-                system("clear");
-                cout<< "Not enough money\n";
-                return true;
+        if (act == 0){
+            if (input == "t"){
+                return false;
+            } else if (input == "g"){
+                if (!withdraw(monsterPrice)){
+                    system("clear");
+                    cout<< "Not enough money\n";
+                    return true;
+                } else {
+                    toiletGold += 1;
+                    monsterPrice  = monsterPrice * 1.08;
+                }
+
+            } else if (input == "q"){
+                exit(1);
+            } else if (input == "m"){
+                if (!withdraw(monsterPrice)){
+                    system("clear");
+                    cout<<"Not Enough Money\n";
+                    return true;
+                } else {
+                    monsters += 3;
+                    monsterPrice = monsterPrice * 1.08;
+                }
             } else {
-                toiletGold += 1;
+                return false;
+            }
+        } else {
+            if (input == "t"){
+                return false;
+            } else if (input == "q"){
+                exit(1);
+            } else if (input == "m"){
+                if (!withdraw(monsterPrice)){
+                    system("clear");
+                    cout<<"Not Enough Money\n";
+                    return true;
+                } else {
+                    monsters += 3;
+                    monsterPrice = monsterPrice * 1.08;
+                }
+            } else if (input == "d"){
+                if (!withdraw(diamondPrice)){
+                    system("clear");
+                    cout << "Not enough money\n";
+                    return true;
+                } else {
+                    diamonds += 1;
+                    diamondPrice = diamondPrice * 1.3;
+                }
+                
+                
+            } else {
+                return false;
             }
 
-        } else if (input == "q"){
-            exit(1);
-        } else if (input == "m"){
-            if (!withdraw(40)){
-                system("clear");
-                cout<<"Not Enough Money\n";
-                return true;
-            } else {
-                monsters += 3;
-            }
+
+        }
+    }
+
+    
+    bool isDone(){
+        if (toiletGold >= 7 && diamonds > 7){
+            return true;
         } else {
             return false;
         }
@@ -67,7 +124,7 @@ public:
 
     void turn(){
         humansKilled = rand() % monsters;
-        money += humansKilled;
+        money += humansKilled * 2;
         unsigned int tmp = rand() % 3;
         if (tmp == 1){
             char tmpS = 7;
@@ -81,7 +138,7 @@ public:
 
     void paperwork(){
         cout << " _______ \n \\_______\\ \n  \\_______\\ \n   \\_______\\ \n    \\_______\\ \n\n";
-        int dis = rand() % 12;
+        int dis = rand() % 20;
         if (dis == 0){
             cout << "You get a paper cut and accidentally give half the monsteres the epock off.\n";
             monsters = monsters - (monsters / 2);
@@ -107,7 +164,7 @@ public:
             money += 15;
         } else if (dis == 6){
             cout << "The pipes start leaking more than Google Chrome\n";
-            money -= 25;
+            money -= 10;
         } else if (dis == 7){
             cout << "You spare a humans life so they can work in your sweatshop making low quality, designer, knockoff wallets.\n";
             money += 2;
@@ -115,7 +172,7 @@ public:
             cout << "You organize the garbage instead of doing paper work.\n";
         } else if (dis == 9){
             cout << "Payroll fucked up your cash.\n";
-            money -= 10;
+            money -= 7;
         } else {
             cout << "Boring paperwork as usual\n";
         }
@@ -131,7 +188,7 @@ public:
         if (tmp == "n"){
             return true;
         } 
-        int dis = rand() % 22;
+        int dis = rand() % 25;
         if (dis == 3){
             money -= 20;
             cout<< "Cracked Cement! Looks like you have to repair the ceiling.\n";
@@ -187,7 +244,14 @@ public:
             cout<< "The grease is in the spoon! I repeat, the grease is in the spoon!\n";
         } else if (dis == 20){
             cout << "You win A BRAND NEW CAR!\n";
+            money += 50;
+        } else if (dis == 21){
+            cout << "Heres your EPA grant!\n";
             money += 20;
+        } else if (dis == 22){
+            cout << "Promotionn!\n";
+            cout << "...\nIt wasn't a real promotion, and you embarass yourself.\n";
+            
         } else {
             cout<< "They hung up\n";
         }
@@ -197,10 +261,15 @@ public:
 
 
 private:
+    unsigned int act;
+    float diamondPrice;
+    float toiletPrice;
+    float monsterPrice;
     unsigned int monsters;
-    int money;
+    float money;
     unsigned int humansKilled;
     unsigned int toiletGold;
+    unsigned int diamonds;
 
 };
 
